0d43c6c835b721eee9070f66e7c03fed11a460d9
LeannePepper <leanne.werner.lw@gmail.com>
2019-12-16 22:13:56 -0800

    add function

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  AppRegistry,
+  TextInput,
+  TouchableHighlight
+} from "react-native";
+import { render } from "react-dom";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>React Native Todo app</Text>
-    </View>
-  );
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      newTodo: ""
+    };
+  }
+
+  handleChange(e) {
+    const { value } = e.target;
+    this.setState({ newTodo: value });
+  }
+  handlePress() {
+    this.setState({
+      todos: [...this.state.todos, this.state.newTodo],
+      newTodo: ""
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          value={this.state.newTodo}
+          onChange={this.handleChange.bind(this)}
+        />
+        <TouchableHighlight onPress={this.handlePress.bind(this)}>
+          <Text>Add Item</Text>
+        </TouchableHighlight>
+        {this.state.todos.map((todo, i) => (
+          <Text key={i}>{todo}</Text>
+        ))}
+      </View>
+    );
+  }
 }
+export default Todo;
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +57,4 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   }
 });
+AppRegistry.registerComponent("Todo", () => Todo);
